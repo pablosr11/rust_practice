b50e796a3341fbcc1aca77280c80d5d00b8b6988
Pablo Sanderson <pablosr11@gmail.com>
2021-09-24 14:25:40 +0100

    take input from std::io, compare to guess, keep looping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing game");
+
+    let random_number = rand::thread_rng().gen_range(0..101);
+    loop {
+        println!("Input noooomber");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read ln");
+
+        // trim the newline added by pressing enter
+        // parse returns a result enum, use it to match
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("TOO SMOL"),
+            Ordering::Greater => println!("TOO BANANA?"),
+            Ordering::Equal => {
+                println!("U WON");
+                break;
+            }
+        }
+    }
 }
