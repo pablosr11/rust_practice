5bcbedb694c5294689ab2973f35ffe9cb6a86c81
Pablo Sanderson <pablosr11@gmail.com>
2021-09-25 13:41:11 +0100

    Read in args, small validation and output filecontents

--- rusty_grep/src/main.rs
+++ rusty_grep/src/main.rs
@@ -1,3 +1,20 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() == 3 {
+        false => Err("Missing filename or query"),
+        true => Ok("yei"),
+    }
+    .expect("missing arg");
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Finding {} in {}", &query, &filename);
+
+    let contents = fs::read_to_string(&filename).expect("Couldnt read the file");
+
+    println!("{}", &contents);
 }
