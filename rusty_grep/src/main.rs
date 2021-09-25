487d17894aabbc140f001708476f6ff5056f21ff
Pablo Sanderson <pablosr11@gmail.com>
2021-09-25 15:29:03 +0100

    Add config struct and implement its constructor

--- rusty_grep/src/main.rs
+++ rusty_grep/src/main.rs
@@ -1,20 +1,34 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    match args.len() == 3 {
-        false => Err("Missing filename or query"),
-        true => Ok("yei"),
-    }
-    .expect("missing arg");
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Issue found parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Finding {} in {}", config.query, config.filename);
 
-    println!("Finding {} in {}", &query, &filename);
-
-    let contents = fs::read_to_string(&filename).expect("Couldnt read the file");
+    let contents = fs::read_to_string(config.filename).expect("Couldnt read the file");
 
     println!("{}", &contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enuff args");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
