fb32496b8601059521a25698b78f734636422719
Pablo Sanderson <pablosr11@gmail.com>
2021-09-27 11:52:30 +0100

    Add insensitive fn depending on ENV var

--- rusty_grep/src/lib.rs
+++ rusty_grep/src/lib.rs
@@ -1,8 +1,9 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub sensitive: bool,
 }
 
 impl Config {
@@ -13,21 +14,33 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        let sensitive = env::var("RUSTY_SENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config {
+            query,
+            filename,
+            sensitive,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    for line in search(&config.query, &contents) {
+
+    let results = if config.sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", &line);
     }
     Ok(())
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&str> = vec![];
+    let mut results: Vec<&str> = Vec::new();
     for line in contents.lines() {
         if line.contains(&query) {
             results.push(&line.trim())
