2af20df146176b41e64b5ce475c40950125f23a5
Pablo Sanderson <pablosr11@gmail.com>
2021-09-27 11:54:30 +0100

    Send errors to the error stream

--- rusty_grep/src/main.rs
+++ rusty_grep/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Issue found parsing arguments: {}", err);
+        eprintln!("Issue found parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = rusty_grep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1)
     }
 }
