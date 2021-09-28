6cb8b0e0dc0f58f98ab2bda0e16bd858b60f7bd3
Pablo Sanderson <pablosr11@gmail.com>
2021-09-28 13:39:32 +0100

    Add the Threadpool managing the workers and the sender end of the channel

--- web_servo/src/lib.rs
+++ web_servo/src/lib.rs
@@ -21,3 +21,38 @@ impl Worker {
         Worker { id, thread }
     }
 }
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for worker_id in 0..size {
+            workers.push(Worker::new(worker_id, Arc::clone(&receiver)))
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
