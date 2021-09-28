7ea7797c344b539de6569d3d5646df23318e43e0
Pablo Sanderson <pablosr11@gmail.com>
2021-09-28 14:37:00 +0100

    Add Enum to diff between execute and kill signals

--- web_servo/src/lib.rs
+++ web_servo/src/lib.rs
@@ -4,6 +4,11 @@ use std::{
     thread,
 };
 
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 struct Worker {
@@ -12,11 +17,20 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker got job: {}, executing", id);
-            job();
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker got job: {}, executing", id);
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker with id {} is terminated", id);
+                    break;
+                }
+            }
         });
         Worker {
             id,
@@ -27,7 +41,7 @@ impl Worker {
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 impl ThreadPool {
@@ -56,12 +70,19 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending shutdown message to all workers");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+        println!("Shutting down all workers");
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
             if let Some(thread) = worker.thread.take() {
